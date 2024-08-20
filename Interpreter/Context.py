8dd2cca135f4d4439b1faadd6c5995aea34dbf6d
lordYorden <87636389+lordYorden@users.noreply.github.com>
2024-08-20 20:27:25 +0300

    added docstring for Context, RuntimeResult and SymbolTable

--- Interpreter/Context.py
+++ Interpreter/Context.py
@@ -1,6 +1,13 @@
 from Interpreter.SymbolTable import SymbolTable
 class Context:
     def __init__(self, display_name, parent=None, parent_entry_position=None):
+        """Initializes the context object
+
+        Args:
+            display_name (String): the name of the context
+            parent (Context, None): the parent context. Defaults to None.
+            parent_entry_position (Position, None): the new context entry position. Defaults to None.
+        """
         self.display_name = display_name
         self.parent = parent
         self.symbol_table = None
@@ -9,6 +16,14 @@ class Context:
         self.parent_entry_position = parent_entry_position
         
     def set_perent(self, parent):
+        """Sets the parent context
+
+        Args:
+            parent (Context): the parent context
+
+        Returns:
+            Context: the current context object
+        """
         self.parent = parent
         return self
         
--- Interpreter/SymbolTable.py
+++ Interpreter/SymbolTable.py
@@ -1,18 +1,42 @@
 class SymbolTable:
     def __init__(self, parent=None):
+        """Initializes the SymbolTable object
+
+        Args:
+            parent (SymbolTable, None): the parent symbol table. Defaults to None.
+        """
         self.symbols = {}
         self.perent = parent
         
     def get(self, name):
+        """Gets the object of a symbol
+
+        Args:
+            name (String): the name of the symbol
+
+        Returns:
+            Object: the object of the symbol
+        """
         value = self.symbols.get(name, None)
         if value == None and self.perent:
             return self.perent.get(name)
         return value
     
     def set(self, name, value):
+        """Sets the object of a symbol
+
+        Args:
+            name (String): the name of the symbol
+            value (Object): the object of the symbol
+        """
         self.symbols[name] = value
         
     def remove(self, name):
+        """Removes a symbol
+
+        Args:
+            name (String): the name of the symbol
+        """
         del self.symbols[name]
         
     
